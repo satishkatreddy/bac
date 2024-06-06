ee69e8aad374c549414707793dd16076ddc615b4
satish <satishkatreddy40@gmail.com>
2024-06-06 16:14:06 +0530

    code is updated

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const {config} = require('dotenv');
+const { config } = require('dotenv');
 const connectedDb = require('../backend/utils/database');
-const vender= require('../backend/routers/vendor');
-const  firm = require('../backend/routers/firm');
+const vender = require('../backend/routers/vendor');
+const firm = require('../backend/routers/firm');
 const product = require('../backend/routers/product');
 const app = express();
 
 
 config({
-    path:'../backend/config.env'
+    path: '../backend/config.env'
 })
 connectedDb();
 
@@ -19,12 +19,12 @@ app.use('/api/product', product);
 app.use('/uploads', express.static('uploads'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, (req,res,err)=>{
+app.listen(PORT, (req, res, err) => {
 
-   if(err){
-    console.log(err);
-   }
-   else{
-    console.log(`server is running on ${PORT}...`)
-   }
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log(`server is running on ${PORT}...`)
+    }
 })
